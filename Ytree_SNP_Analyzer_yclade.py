76a420f286708d6379c3eaf2e7be8bce8669d318
Dr. Simcha Gershon Bohrer (Ph.D) <108482960+sgbmzm@users.noreply.github.com>
2025-09-18 23:51:04 +0300

    Update Ytree_SNP_Analyzer_yclade.py

--- Ytree_SNP_Analyzer_yclade.py
+++ Ytree_SNP_Analyzer_yclade.py
@@ -24,6 +24,19 @@ last_dna_file_type = ""
 last_yfull_link = ""
 last_ftdna_link = ""
 
+
+# פונקצייה לקבל מיקום מוחלט של קובץ שאמור להיות כלול בתוכנה
+# ראו: https://stackoverflow.com/questions/7674790/bundling-data-files-with-pyinstaller-onefile/44352931#44352931
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 # ------------------------
 # איפוס משתמש
 # ------------------------
