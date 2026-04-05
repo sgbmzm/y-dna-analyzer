88fec9871d4ef0b43dc719d6fce584fd8343fa55
Dr. Simcha Gershon Bohrer (Ph.D) <108482960+sgbmzm@users.noreply.github.com>
2026-04-05 20:01:30 +0300

    Fix filename format for saving clade results

--- y-dna-analyzer.py
+++ y-dna-analyzer.py
@@ -1225,7 +1225,7 @@ def save_clades_to_file():
         messagebox.showerror("save_clades_to_file Error", "No Clades to save.")
         return
     file_path = filedialog.asksaveasfilename(
-        initialfile=f"{os.path.basename(last_dna_file) if last_dna_file else getattr(last_clades[0], 'name', '')}.yclade_results.txt",
+        initialfile=f"{os.path.basename(last_dna_file) if last_dna_file else getattr(last_clades[0], 'name', '')}_yclade_results.txt",
         defaultextension=".txt",
         filetypes=[("Text files", "*.txt"), ("All files", "*.*")]
     )
